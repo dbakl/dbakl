acdc3b5a9c344e1ccbc97761f6f6e8df97b77c38
matthes <matthes@leuffen.de>
2016-09-02 21:05:36 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # dbakl
 All in one development kit
+
+
+## Usage
+
+### Connecting the Database
+Setting up the Connection
+
+```
+$con = new MySqlConnection(new MySqlDriver());
+$con->connect();
+```
+
+
+### Persistence Manager
+
+```
+$pm->load($e = new SomeEntity(), ["id"=>"SomeValue"]);
+$pm->store($e);
+$pm->delete($e);
+```
+
+
+### FluentSql
+
+```
+$pm->fsql()->select()->from(Entity::class, "a")->leftJoin(Entity2::class, "b")->then()->each(
+    function (Entity $e, Entity2 $e2) {
+    }
+);
+```
+
+or - more abstract - let the framework build the object
+
+```
+$pm->fsql()->select()->from(Entity::class, "a")->leftJoin(Entity2::class, "b", [Entity::class, "bs", true])->then()->each(
+    function 
+);
+```
+
+will fill the Entity Objects property "bs" with Entities.
+
+
+### Upgrade a struct
+
+```
+$pm->store($entity);
+```
+
+
---
+++ src/dbakl-fsql/src/FSql/FSqlExpr.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthes
+ * Date: 02.09.16
+ * Time: 20:57
+ */
+
+namespace DbAkl\Com\FSql;
+
+
+class FSqlExpr
+{
+
+    public function or() {
+
+    }
+
+    public function eq() {
+
+    }
+
+    public function like() {
+
+    }
+
+    // Example - $qb->expr()->neq('u.id', '?1') => u.id <> ?1
+    public function neq($x, $y); // Returns Expr\Comparison instance
+
+    // Example - $qb->expr()->lt('u.id', '?1') => u.id < ?1
+    public function lt($x, $y); // Returns Expr\Comparison instance
+
+    // Example - $qb->expr()->lte('u.id', '?1') => u.id <= ?1
+    public function lte($x, $y); // Returns Expr\Comparison instance
+
+    // Example - $qb->expr()->gt('u.id', '?1') => u.id > ?1
+    public function gt($x, $y); // Returns Expr\Comparison instance
+
+    // Example - $qb->expr()->gte('u.id', '?1') => u.id >= ?1
+    public function gte($x, $y); // Returns Expr\Comparison instance
+
+    // Example - $qb->expr()->isNull('u.id') => u.id IS NULL
+    public function isNull($x); // Returns string
+
+    // Example - $qb->expr()->isNotNull('u.id') => u.id IS NOT NULL
+    public function isNotNull($x); // Returns string
+
+
+}
